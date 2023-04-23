e854eafa1a03d1407cfb24d941448840703ddc2c
Kensuke <kenshimojo@icloud.com>
2023-04-23 00:19:44 -0700

    ADDED GREYNESS VARIABLE
    basically pixel color val is chosen within 50 above 0 or below 255 (max), but higher that "50", the more grey and less ibnary it lokds

--- src/staticScreenSrc.java
+++ src/staticScreenSrc.java
@@ -9,16 +9,21 @@ import java.awt.event.ActionListener;
 
 public class staticScreenSrc extends JPanel {
 	Timer timer;
-	int sandSize=1;
+	int sandSize=1;//2 or 1 best
+	int greyness=50;//after 100, chooses value within 100 above 0 and below 255
 	public void paintComponent(Graphics g) {
 		// Rectangle bounds=g.getClipBounds();
 		super.paintComponent(g);
 		for (int x = 0; x < getWidth(); x=x+sandSize) {
 			for (int y = 0; y < getHeight(); y=y+sandSize) {
-				int col = (int) (Math.round(Math.random() * 100) % 50);
+				int col = (int) (Math.round(Math.random() * 100) % greyness);
+				//System.out.println(col);
+				//WARNING PRINTING LIMITS REFRESH RATE
 				if (Math.random() > 0.5) {
 					col = 255 - col;
 				}
+				
+				
 				g.setColor(new Color(col, col, col));
 				g.fillRect(x, y, sandSize,sandSize);
 			}
@@ -27,7 +32,7 @@ public class staticScreenSrc extends JPanel {
 
 	public static void main(String[] args) {
 
-		final int INTERVAL = 8;//msec
+		final int INTERVAL = 17;//msec 17 for 60 fps
 
 		var panel = new staticScreenSrc();
 
@@ -40,7 +45,7 @@ public class staticScreenSrc extends JPanel {
 		Timer timer = new Timer(INTERVAL, new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				// Refresh the panel
-				//panel.revalidate();
+				panel.revalidate();
 				panel.repaint();
 			}
 		});

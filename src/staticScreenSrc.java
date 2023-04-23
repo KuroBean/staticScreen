698787a6e48982209975c15d9a905f7c27a79729
Kensuke <kenshimojo@icloud.com>
2023-04-22 23:39:20 -0700

    SANDSIZE CHANGE

--- src/staticScreenSrc.java
+++ src/staticScreenSrc.java
@@ -9,25 +9,25 @@ import java.awt.event.ActionListener;
 
 public class staticScreenSrc extends JPanel {
 	Timer timer;
-
+	int sandSize=1;
 	public void paintComponent(Graphics g) {
 		// Rectangle bounds=g.getClipBounds();
 		super.paintComponent(g);
-		for (int x = 0; x < getWidth(); x++) {
-			for (int y = 0; y < getHeight(); y++) {
+		for (int x = 0; x < getWidth(); x=x+sandSize) {
+			for (int y = 0; y < getHeight(); y=y+sandSize) {
 				int col = (int) (Math.round(Math.random() * 100) % 50);
 				if (Math.random() > 0.5) {
 					col = 255 - col;
 				}
 				g.setColor(new Color(col, col, col));
-				g.fillRect(x, y, 1, 1);
+				g.fillRect(x, y, sandSize,sandSize);
 			}
 		}
 	}
 
 	public static void main(String[] args) {
 
-		final int INTERVAL = 1000;
+		final int INTERVAL = 8;//msec
 
 		var panel = new staticScreenSrc();
 
@@ -40,7 +40,7 @@ public class staticScreenSrc extends JPanel {
 		Timer timer = new Timer(INTERVAL, new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				// Refresh the panel
-				panel.revalidate();
+				//panel.revalidate();
 				panel.repaint();
 			}
 		});

aae042908fba9e8083f44975e4d01c8b3988a86a
shin <sshimojo@caltech.edu>
2023-04-25 00:30:24 -0700

    code optimized for speed

--- src/staticScreenSrc.java
+++ src/staticScreenSrc.java
@@ -9,19 +9,29 @@ import java.awt.event.ActionListener;
 
 public class staticScreenSrc extends JPanel {
 	Timer timer;
-	int sandSize=1
-			;//2 or 1 best
-	int greyness=100;//after 100, chooses value within 100 above 0 and below 255
+	
+	
+	int sandSize=2;//2 or 1 best
+	int greyness=50;//2 for black or white only, after 100, chooses value within 100 above 0 and below 255
+	
+	
 	public void paintComponent(Graphics g) {
 		// Rectangle bounds=g.getClipBounds();
 		super.paintComponent(g);
 		for (int x = 0; x < getWidth(); x=x+sandSize) {
 			for (int y = 0; y < getHeight(); y=y+sandSize) {
+				/**
 				int col = (int) (Math.round(Math.random() * 100) % greyness);
 				//System.out.println(col);
 				//WARNING PRINTING LIMITS REFRESH RATE
 				if (Math.random() > 0.5) {
 					col = 255 - col;
+				}*/
+				int col;
+				if (Math.random() > 0.5) {//white chosen
+					col = 205;
+				}else {//black chosen
+					col=50;
 				}
 				
 				g.setColor(new Color(col, col, col));
@@ -31,11 +41,10 @@ public class staticScreenSrc extends JPanel {
 	}
 
 	public static void main(String[] args) {
+		
 		final int screenWidth=500;
-		final int screenHeight=500;
-		
-		
-		final int INTERVAL = 17;//msec 17 for 60 fps
+		final int screenHeight=500;	
+		final int INTERVAL = 17;//miniumm is 17 mmsec for 60 fps
 
 		var panel = new staticScreenSrc();
 
